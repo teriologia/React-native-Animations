9046d636c3e6d82fbff20577bc6b74b0c7470816
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-12-13 23:38:04 +0300

    simple Fix

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react';
 import First from './src/first';
 import Second from './src/second';
-import Three from './src/three';
+// import Three from './src/three'; IN PROGRESS
 import Four from './src/Four';
 
 class App extends Component {
